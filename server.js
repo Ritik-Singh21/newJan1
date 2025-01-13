b0bc9b332701eb69cf0b3fa0c26f4503e029ee0f
Ritik Singh <77534764+Ritik-Singh21@users.noreply.github.com>
2025-01-13 18:50:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT=3003;
 app.get('/',(req, res)=>{
   res.send("coding first");
-  )};
+});
 app.listen(PORT,()=>{
   console.log("Hello server start running now ");
-  )};
+});
