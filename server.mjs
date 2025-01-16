36107a1c5c906f52139c6d1582f47e70dc444033
Ritik Singh <77534764+Ritik-Singh21@users.noreply.github.com>
2025-01-17 00:51:31 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const PORT=3003;
 app.get('/',(req, res)=>{
-  res.send("coding karo !!!");
+  res.send("Kya bro Code karo !!");
   });
 app.listen(PORT,()=>{
   console.log("Hello server start running now! ");
